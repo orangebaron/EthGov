ca776ad3277e3d130f02c3887102677413c2cefb
Orange Baron <orangebaronroblox@gmail.com>
2017-02-06 22:45:22 -0500

    added update function

--- Contracts.sol
+++ Contracts.sol
@@ -5,9 +5,9 @@ contract OrangeGov_Main {
     
 	mapping(address=>mapping(string=>bool)) permissions;
 	mapping(address=>mapping(string=>bool)) userStatuses;
-	mapping(string=>address) addresses;
 	mapping(string=>address) contractIDs;
 	mapping(string=>bool) contractIDExists;
+	address[] contractArray; //the contracts in the above 2 tables
 	function getHasPermission(address user, string permissionName, string userStatusAllowed) returns (bool hasPermission){ //for use between contracts
 	    return permissions[msg.sender][permissionName]||permissions[msg.sender]["all"]||userStatuses[msg.sender][userStatusAllowed];
 	}
@@ -22,6 +22,16 @@ contract OrangeGov_Main {
         }
     }
     
+    function removeFromContractIDArray(address contractToRemove) {
+        for (uint x=0;x<contractArray.length-1;x++) {
+            if (contractArray[x]==contractToRemove) {
+                contractArray[x]=contractArray[contractArray.length-1];
+	            contractArray.length--;
+	            return;
+            }
+        }
+    }
+    
 	function addContract(string ID,bytes code) permissionRequired("addContract",""){
 	    address addr;
         assembly {
@@ -31,16 +41,32 @@ contract OrangeGov_Main {
         address oldAddr = contractIDs[ID];
 	    contractIDs[ID]=addr;
 	    contractIDExists[ID]=true;
-	    //warnings below are irrelevant
 	    oldAddr.call.gas(msg.gas)(bytes4(sha3("changeCurrentContract(address)")),addr); //if there was a previous contract, tell it the new one's address
 	    addr.call.gas(msg.gas)(bytes4(sha3("tellPreviousContract(address)")),oldAddr); //feed it the address of the previous contract
+	    removeFromContractIDArray(addr);
+	    contractArray.length++;
+	    contractArray[contractArray.length-1]=addr;
 	}
 	function removeContract(string ID) permissionRequired("removeContract",""){
 	    contractIDExists[ID]=false;
-	    //warning below is irrelevant
 	    contractIDs[ID].call.gas(msg.gas)(bytes4(sha3("changeCurrentContract(address)")),currentContract); //make sure people using know it's out of service
+	    removeFromContractIDArray(contractIDs[ID]);
 	}
-	//TO DO HERE: UPDATE
+	function update(bytes code) permissionRequired("update",""){
+	    address addr;
+        assembly {
+            addr := create(0,add(code,0x20), mload(code))
+            jumpi(invalidJumpLabel,iszero(extcodesize(addr)))
+        }
+        addr.call.gas(msg.gas)(bytes4(sha3("tellPreviousContract(address)")),currentContract);
+        currentContract = addr;
+        for (uint x=0;x<contractArray.length-1;x++) {
+            contractArray[x].call.gas(msg.gas)(bytes4(sha3("changeMain(address)")),currentContract);
+        }
+	}
+	function tellPreviousContract(address prev) { //called in the update process
+	    
+	}
 	function spendEther(address addr, uint256 weiAmt) permissionRequired("spendEther",""){
 	    if (!addr.send(weiAmt)) throw;
 	}
