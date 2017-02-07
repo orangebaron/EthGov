21d6a92fd397912cf27e0f4d9c32c1b370327e64
Orange Baron <orangebaronroblox@gmail.com>
2017-02-06 22:09:05 -0500

    whoops forgot this one
    made permission checking use the .call method

--- Contracts.sol
+++ Contracts.sol
@@ -69,7 +69,7 @@ contract OrangeGov_Template {
     }
     modifier permissionRequired(string permissionName,string userStatusAllowed) { //userStatusAllowed - a certain user status is the only thing necessary to run the function
         _;
-        if (!OrangeGov_Main(main).getHasPermission(msg.sender,permissionName,userStatusAllowed)){
+        if (!main.call.gas(msg.gas)(bytes4(sha3("getHasPermission(address,string,string) returns bool")),msg.sender,permissionName,userStatusAllowed)){
             throw;
         }
     }
